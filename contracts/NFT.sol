f6383136b441fcb371abae31340ac4d73e793c43
NicoCasal <martin.casal.venencia@gmail.com>
2022-11-22 16:40:59 -0300

    second commit

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -7,4 +7,10 @@ contract NFT is ERC721URIStorage{
     uint public tokenCount;
 
     constructor() ERC721("DApp NFT", "DApp"){}
+    function mint(string memory _tokenURI) external retunrs (uint){
+        tokenCount++;
+        _safeMint(msg.sender, tokenCount);
+        _setTokenURI(tokenCount, _tokenURI);
+        return tokenCount;
+    }
 }
